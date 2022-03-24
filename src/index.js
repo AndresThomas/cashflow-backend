f3d06e9752b6b3b283deca1a15afe60186dacdc1
AndresThomas <173218@ids.upchiapas.edu.mx>
2022-03-23 20:44:47 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app=express();
 const morgan = require('morgan');  
 
 //settings
-app.set('port', process.env.PORT ||3000);
+app.set('port', process.env.PORT ||3000||5000);
 app.set('json spaces',2);
 
 //middleware //combinated
@@ -13,7 +13,14 @@ app.use(express.json());
 
 app.use(require('./routes/index'));
 
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 //server init
-app.listen(3000,()=>{
+app.listen(app.get('port'),()=>{
     console.log('Hola mundo ',app.get('port'));
 });
