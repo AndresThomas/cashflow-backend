cb396400d071411ff6335c0160264c74dc176ce1
AndresThomas <173218@ids.upchiapas.edu.mx>
2022-03-23 20:46:10 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -13,13 +13,6 @@ app.use(express.json());
 
 app.use(require('./routes/index'));
 
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-
 //server init
 app.listen(app.get('port'),()=>{
     console.log('Hola mundo ',app.get('port'));
