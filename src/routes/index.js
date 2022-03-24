05571f572a53a7bf9bee62b07649a2336ac3ed29
AndresThomas <173218@ids.upchiapas.edu.mx>
2022-03-24 15:49:46 -0600

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -59,55 +59,49 @@ router.post("/categorias", (request, response) => {
   console.log(request.body);
   if (clasificacion && categoria && subCategoria) {
     const newCategoria = { ...request.body };
-    console.log('Aproved');
+    console.log("Aproved");
     conexion.query(
       "Insert into Categoria (categoria,clasificacion,subCategoria) values (?,?,?)",
       [categoria, clasificacion, subCategoria],
       function (error, results, fields) {
         response.json(results);
-        console.log(request.body,' post');
+        console.log(request.body, " post");
         if (error) {
           throw error;
         }
-        
       }
     );
   }
 });
 
 router.post("/flujo", (request, response) => {
-  const { fecha, tipo, categoria, descripcion, cantidad } = request.body;
   console.log(request.body);
-  console.log(fecha);
-  console.log(tipo);
-  console.log(categoria);
-  console.log(descripcion);
-  console.log(cantidad);
-  if (fecha && tipo && categoria && descripcion && cantidad) {
-    const newFlujo = { ...request.body };
-    console.log('Aproved');
-    conexion.query(
-      "Insert into FlujoEfectivo (fecha,categoria,descripcion,monto) values (?,?,?,?)",
-      [fecha, categoria, descripcion, cantidad],
-      function (error, results, fields) {
-        console.log(request.body,' post');
-        if (error) throw error;
-        response.json(results);
-      }
-    );
-  }
+  
+  const newFlujo = { ...request.body };
+  print(newFlujo);
+  console.log("Aproved");
+  conexion.query(
+    "Insert into FlujoEfectivo (fecha,categoria,descripcion,monto) values (?,?,?,?)",
+    [fecha, categoria, descripcion, cantidad],
+    function (error, results, fields) {
+      console.log(request.body, " post");
+      if (error) throw error;
+      response.json(results);
+    }
+  );
 });
 router.post("/indicadores", (request, response) => {
-  const {  tipoIndicador, numeroSemana,razonSocial,monto, fecha } = request.body;
+  const { tipoIndicador, numeroSemana, razonSocial, monto, fecha } =
+    request.body;
   console.log(request.body);
   if (fecha && tipoIndicador && numeroSemana && razonSocial && monto) {
     const newInd = { ...request.body };
-    console.log('Aproved');
+    console.log("Aproved");
     conexion.query(
       "Insert into IndicadoresDinero (tipoIndicador,numeroSemana,razonSocial,monto,fecha) values (?,?,?,?,?)",
-      [tipoIndicador, numeroSemana, razonSocial, monto,fecha],
+      [tipoIndicador, numeroSemana, razonSocial, monto, fecha],
       function (error, results, fields) {
-        console.log(request.body,' post');
+        console.log(request.body, " post");
         if (error) throw error;
         response.json(results);
       }
